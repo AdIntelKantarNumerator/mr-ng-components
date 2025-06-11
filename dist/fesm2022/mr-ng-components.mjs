211bfb003e576890f4078d6e55e680b09f9ef7b3
Kishor Pardhi <kishor.pardhi@ad.vivvix.com>
2025-06-11 15:10:26 +0530

    Initial library commit

--- dist/fesm2022/mr-ng-components.mjs
+++ dist/fesm2022/mr-ng-components.mjs
@@ -1,5 +1,6 @@
 import * as i0 from '@angular/core';
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 class MrNgComponents {
     static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "19.2.14", ngImport: i0, type: MrNgComponents, deps: [], target: i0.ɵɵFactoryTarget.Component });
@@ -7,15 +8,15 @@ class MrNgComponents {
     <p>
       mr-ng-components works!
     </p>
-  `, isInline: true, styles: [""] });
+  `, isInline: true, styles: [":host{display:block}\n"], dependencies: [{ kind: "ngmodule", type: CommonModule }] });
 }
 i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "19.2.14", ngImport: i0, type: MrNgComponents, decorators: [{
             type: Component,
-            args: [{ selector: 'lib-mr-ng-components', imports: [], template: `
+            args: [{ selector: 'lib-mr-ng-components', standalone: true, imports: [CommonModule], template: `
     <p>
       mr-ng-components works!
     </p>
-  ` }]
+  `, styles: [":host{display:block}\n"] }]
         }] });
 
 /*
--- src/lib/mr-ng-components.ts
+++ src/lib/mr-ng-components.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'lib-mr-ng-components',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   template: `
     <p>
       mr-ng-components works!
     </p>
   `,
-  styles: ``
+  styles: [`:host { display: block; }`]
 })
 export class MrNgComponents {
 
